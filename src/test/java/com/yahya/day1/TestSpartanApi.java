801ad60f5fc17045e74129cc90a84aa186dfc3a1
yahya09206 <yahyahussein91@gmail.com>
2025-04-02 13:03:32 -0700

    initial commit

---
+++ src/test/java/com/yahya/day1/TestSpartanApi.java
@@ -0,0 +1,31 @@
+package com.yahya.day1;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+
+public class TestSpartanApi {
+
+    @Test
+    public void test(){
+        System.out.println("Hello World");
+
+        // Send request to below url and save the response
+        // GET method is coming from RestAssured class to send get request to the url defined
+        // Store request in response object
+
+        // Save result of url into Response object
+        Response response = get("http://44.211.192.252:8000/api/hello");
+        // Print out statusCode from response object -- should return 200(ok)
+        System.out.println("response.statusCode() = " + response.statusCode());
+
+        // Use prettyPrint to print from what's inside payload
+        response.prettyPrint();
+
+        // Assert that response code is indeed a 200 using Assertions class
+        Assertions.assertEquals(200, response.statusCode());
+
+
+    }
+}
