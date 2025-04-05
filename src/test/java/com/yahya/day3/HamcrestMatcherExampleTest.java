cb03e44d3e0d874dc945a41881241e734e38d838
Yahya <yahyahussein91@gmail.com>
2025-04-05 01:39:02 -0700

    hamcrest matcher crash course

---
+++ src/test/java/com/yahya/day3/HamcrestMatcherExampleTest.java
@@ -0,0 +1,39 @@
+package com.yahya.day3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HamcrestMatcherExampleTest {
+
+    /**
+     * Hamcrest is a framework for writing matcher objects allowing match rules
+     * to be defined declaratively
+     * It is an assertion library that can be used additionally to make assertions
+     * readable, and it comes with a lot of pre-written matchers to make it easier to write and read
+     *
+     */
+
+    @Test
+    public void testNumbers(){
+
+        // Junit5 assertion
+        assertEquals(9, 3 + 6);
+
+        // Hamcrest reads more like human language
+        assertThat(3 + 6, equalTo(9));
+
+        // Hamcrest uses matches to express the intention clearly
+        // for equality, there are options like
+        /**
+         * equalTo(some value)
+         * is(some value)
+         * is(equalTo(someValue) // just for readability
+         */
+        assertThat(4+6, is(10));
+        assertThat(5 + 6, is(greaterThan(10)));
+    }
+
+}
