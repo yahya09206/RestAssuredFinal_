a58b14e8cb14c914576f075edd75a5b912d4b013
yahya09206 <yahyahussein91@gmail.com>
2025-04-07 11:59:23 -0700

    serialization and deserialization test

---
+++ src/test/java/com/yahya/day5/PostRequestWithObjectTest.java
@@ -0,0 +1,27 @@
+package com.yahya.day5;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Serialization: Converting from Java Object to JSON(Or any other text)
+ * De-Serialization: Process of Converting from Json to Java Object
+ */
+
+
+public class PostRequestWithObjectTest {
+
+    /**
+     * POST /spartans
+     * content type is json
+     * body is
+     *  {
+     *     "name": "Meade",
+     *     "gender": "Male",
+     *     "phone": 3584128232
+     * }
+     *
+     */
+}
