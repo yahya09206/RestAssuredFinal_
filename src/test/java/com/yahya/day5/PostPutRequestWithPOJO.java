9967ebe9a7c68c419d196ab87921cdd7bcca43b1
yahya09206 <yahyahussein91@gmail.com>
2025-04-07 13:28:30 -0700

    setting up pojo class for spartans

--- src/test/java/com/yahya/day5/PostPutRequestWithPOJO.java
+++ src/test/java/com/yahya/day5/PostPutRequestWithPOJO.java
@@ -3,4 +3,11 @@ package com.yahya.day5;
 public class PostPutRequestWithPOJO {
 
 
+    /**
+     * Another way of representing json data is using special purpose Java Object created from a class that has fields
+     * matched to json keys
+     * and have getters and setters
+     * This type of object's sole purpose is to represent data known as POJO --> plain old java object
+     * The class to create POJO known as a POJO class
+     */
 }
---
+++ src/test/java/com/yahya/pojo/Spartan.java
@@ -0,0 +1,38 @@
+package com.yahya.pojo;
+
+public class Spartan {
+
+    private String name;
+    private String gender;
+    private long phoneNumber;
+
+    public Spartan(String name, String gender, long phoneNumber) {
+        this.name = name;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
