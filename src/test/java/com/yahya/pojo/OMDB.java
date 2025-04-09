14b203c364c1cd0d8580e6b71526cc8ce3e1573c
yahya09206 <yahyahussein91@gmail.com>
2025-04-09 09:22:43 -0700

    added getters setters and toString

--- src/test/java/com/yahya/pojo/OMDB.java
+++ src/test/java/com/yahya/pojo/OMDB.java
@@ -7,4 +7,55 @@ public class OMDB {
     private String imdbID;
     private String Type;
     private String Poster;
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getYear() {
+        return Year;
+    }
+
+    public void setYear(String year) {
+        Year = year;
+    }
+
+    public String getImdbID() {
+        return imdbID;
+    }
+
+    public void setImdbID(String imdbID) {
+        this.imdbID = imdbID;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+
+    public String getPoster() {
+        return Poster;
+    }
+
+    public void setPoster(String poster) {
+        Poster = poster;
+    }
+
+    @Override
+    public String toString() {
+        return "OMDB{" +
+                "Title='" + Title + '\'' +
+                ", Year='" + Year + '\'' +
+                ", imdbID='" + imdbID + '\'' +
+                ", Type='" + Type + '\'' +
+                ", Poster='" + Poster + '\'' +
+                '}';
+    }
 }
