032b3f4a8b4a1413218b75e29591d23cd0c62fa2
yahya09206 <yahyahussein91@gmail.com>
2025-04-09 10:05:45 -0700

    request to get all jobs

---
+++ src/test/java/com/yahya/day6/HR_ORDS_Test.java
@@ -0,0 +1,18 @@
+package com.yahya.day6;
+
+import com.yahya.utility.HRORDSTestBase;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class HR_ORDS_Test extends HRORDSTestBase {
+
+    @Test
+    public void testJobs(){
+
+        JsonPath jsonPath = given().log().uri().when().get("/jobs").prettyPeek().jsonPath();
+    }
+}
