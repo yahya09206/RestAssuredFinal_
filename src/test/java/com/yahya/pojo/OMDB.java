5a355f5ca337853bf53d96fbea8b862291f53400
yahya09206 <yahyahussein91@gmail.com>
2025-04-09 09:21:50 -0700

    setting up pojo class for imdb

---
+++ src/test/java/com/yahya/pojo/OMDB.java
@@ -0,0 +1,10 @@
+package com.yahya.pojo;
+
+public class OMDB {
+
+    private String Title;
+    private String Year;
+    private String imdbID;
+    private String Type;
+    private String Poster;
+}
