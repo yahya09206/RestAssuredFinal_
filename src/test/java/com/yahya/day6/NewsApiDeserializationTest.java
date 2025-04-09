109c6e63ec71e8f4ef7de148cc8fa9eaef555191
yahya09206 <yahyahussein91@gmail.com>
2025-04-09 13:07:58 -0700

    setting up test for news api

---
+++ src/test/java/com/yahya/day6/NewsApiDeserializationTest.java
@@ -0,0 +1,34 @@
+package com.yahya.day6;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import io.restassured.path.json.JsonPath;
+import static io.restassured.RestAssured.*;
+
+
+public class NewsApiDeserializationTest {
+
+    @BeforeAll
+    public static void setUp(){
+        baseURI="https://newsapi.org";
+        basePath="/v2";
+    }
+
+    @AfterAll
+    public static void teardown(){
+        reset();
+    }
+
+    /**
+     * GET https://newsapi.org/v2/top-headlines?country=us
+     * Header : Authorization = Bearer 0b42f37dd32d4c70911a063c275005b0
+     */
+    @Test
+    public void testNews(){
+
+        JsonPath jsonPath = given().log().all().queryParam("country", "us")
+                .header("Authorization", "Bearer = 0b42f37dd32d4c70911a063c275005b0").
+                when().get("/top-headlines").prettyPeek().jsonPath();
+    }
+}
