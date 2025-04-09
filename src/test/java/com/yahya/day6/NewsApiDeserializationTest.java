64440e7dadef0a202ac9ebe225e6d444b12eb9a2
yahya09206 <yahyahussein91@gmail.com>
2025-04-09 13:13:48 -0700

    getting first article

--- src/test/java/com/yahya/day6/NewsApiDeserializationTest.java
+++ src/test/java/com/yahya/day6/NewsApiDeserializationTest.java
@@ -1,5 +1,6 @@
 package com.yahya.day6;
 
+import com.yahya.pojo.Article;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -29,6 +30,12 @@ public class NewsApiDeserializationTest {
 
         JsonPath jsonPath = given().log().all().queryParam("country", "us")
                 .header("Authorization", "Bearer = 0b42f37dd32d4c70911a063c275005b0").
-                when().get("/top-headlines").prettyPeek().jsonPath();
+                when().get("/top-headlines")
+                //.prettyPeek()
+                .jsonPath();
+
+        // try to get first Article into POJO
+        Article articleOne = jsonPath.getObject("articles[0]", Article.class);
+        System.out.println("articleOne = " + articleOne);
     }
 }
